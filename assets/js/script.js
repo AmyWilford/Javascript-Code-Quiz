a3cbc5549631f9fd645811343f9fdb3672bdc6d6
Amy Wilford <amywilford@Amys-MacBook-Air.local>
2022-09-19 16:15:04 -0400

    setTimer function created

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,54 @@ let questionEl = document.getElementById('question');
 let multipleChoiceEl = document.getElementById('multiple-choice');
 let validateEl = document.getElementById('validate')
 
+let questions = [
 
-// let Questions = OBJECT
+]
+let score;
+let isWin = false;
+let timer;
+let countdown;
+
+
 
 // Function to set Timer
-
-
+function setTimer(){
+    countdown=10;
+    timer = setInterval(function(){
+        countdownEl.textContent = countdown;
+        countdown--;
+        if (countdown >=0) {
+            if (isWin && countdown >0) {
+                clearInterval(timer);
+                // winGame();
+            }
+        }
+        if (countdown === 0) {
+            clearInterval(timer);
+            countdownEl.textContent ='Your time is up!'
+            // looseGame();
+        }
+    },1000)
+}
 // Function to show questions
 // function showQuestions{}
 
 // when right > show new question
 // when wrong > deduct time from the clock
 
-// Function to determine end of time/end of game
-// function gameEnd(){}
+// Function to determine game win
+// function winGame(){}
+
+// Function to determine game loss
+// function loseGame() {}
+
 
 // Function save initials and score
 // function saveInfo(){}
 
 // Function to start the Game
-// function startGame(){}
+function startGame(){
+    startButtonEl.addEventListener('click', setTimer);
+}
+
+startGame();
