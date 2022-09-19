793beb9ed1f47e067e1022d453662c7667a5f2c3
Amy Wilford <amywilford@Amys-MacBook-Air.local>
2022-09-19 15:43:39 -0400

    added function strucutre and code layout in comments

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// let Questions = OBJECT
+
+// Function to set Timer
+// function setTimer(){}
+
+// Function to show questions
+// function showQuestions{}
+
+// when right > show new question
+// when wrong > deduct time from the clock
+
+// Function to determine end of time/end of game
+// function gameEnd(){}
+
+// Function save initials and score
+// function saveInfo(){}
+
+// Function to start the Game
+// function startGame(){}
